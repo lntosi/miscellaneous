5c1d900eb30ea3c03584235ae868fdca9de85521
Lucas N Tosi <47820956+lntosi@users.noreply.github.com>
2023-12-22 11:51:59 +0100

    Add files via upload

---
+++ rsync_nas.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SOURCE_SERVER="192.168.1.x"
+SOURCE_DIR="/home/vboxuser/Desktop/rsync_test_src/"
+
+DESTINATION_SERVER="192.168.1.y"
+DESTINATION_DIR="/home/vboxuser/Desktop/rsync_test_dst/"
+
+USER="type_your_user"
+PASSWORD="type_your_password"
+
+WAIT_TIME=1m  # Wait time after a failure (e.g., 1 minute)
+
+while true; do
+    echo "Checking the availability of the remote server..."
+    if sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USER@$SOURCE_SERVER" "exit"; then
+        echo "The remote server is accessible. Initiating synchronization..."
+        sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USER@$SOURCE_SERVER" "time rsync -avz --delete -e 'sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no' $SOURCE_DIR $USER@$DESTINATION_SERVER:$DESTINATION_DIR"
+        echo "Synchronization completed. Waiting before the next execution..."
+    else
+        echo "Failed to check the availability of the remote server. Waiting before trying again..."
+    fi
+    sleep $WAIT_TIME
+done
